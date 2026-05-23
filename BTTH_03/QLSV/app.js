9a29bb11e559520649a0a71cf42ebbc48b3eb95b
Vu Thu Thuy <huongdongnat1980@gmail.com>
2026-05-23 14:49:33 +0700

    Hoàn thành giai đoạn 3

--- BTTH_03/QLSV/app.js
+++ BTTH_03/QLSV/app.js
@@ -6,13 +6,119 @@ const studentList = document.getElementById("studentList");
 const message = document.getElementById("message");
 const totalStudents = document.getElementById("totalStudents");
 const averageScore = document.getElementById("averageScore");
+/* =========================
+   MẢNG DỮ LIỆU
+========================= */
 
-// Mở modal
+let students = [];
+
+/* =========================
+   MỞ MODAL
+========================= */
 openModalBtn.addEventListener("click", function(){
     modal.classList.remove("hidden"); // hiển thị modal
 });
 
-// Đóng modal
+/* =========================
+   ĐÓNG MODAL
+========================= */
 closeModalBtn.addEventListener("click", function(){
     modal.classList.add("hidden"); // ẩn modal
 });
+
+studentForm.addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    /* =========================
+       LẤY DỮ LIỆU INPUT
+    ========================= */
+    const id = document.getElementById("studentId").value;
+
+    const name = document.getElementById("studentName").value;
+
+    const dob = document.getElementById("studentDob").value;
+
+    const studentClass = document.getElementById("studentClass").value;
+
+    const score = document.getElementById("studentScore").value;
+
+    const email = document.getElementById("studentEmail").value;
+
+    /* =========================
+       TẠO OBJECT SINH VIÊN
+    ========================= */
+
+    const student = {
+
+        id: id,
+
+        name: name,
+
+        dob: dob,
+
+        studentClass: studentClass,
+
+        score: score,
+
+        email: email
+
+    };
+    students.push(student);
+    renderStudents();
+    console.log(students);
+    console.log(id);
+    console.log(name);
+    console.log("Đã submit form");
+
+});
+
+function renderStudents(){
+
+    studentList.innerHTML = "";
+
+    students.forEach(function(student){
+
+        const row = `
+        
+            <tr>
+
+                <td>${student.id}</td>
+
+                <td>${student.name}</td>
+
+                <td>${student.dob}</td>
+
+                <td>${student.studentClass}</td>
+
+                <td>${student.score}</td>
+
+                <td>${student.email}</td>
+
+                <td>
+
+                    <button>Sửa</button>
+
+                    <button>Xóa</button>
+
+                </td>
+
+            </tr>
+
+        `;
+
+        studentList.innerHTML += row;
+
+    });
+
+    students.push(student);
+
+    renderStudents();
+
+    modal.classList.add("hidden");
+
+    studentForm.reset();
+
+}
+
+
